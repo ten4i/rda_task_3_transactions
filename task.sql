373544fcc905aeccf126cd24a2658d7dd3a5db52
ten4i <oleksijchunihin@gmail.com>
2026-05-09 21:38:31 +0000

    moved insert into transaction

--- task.sql
+++ task.sql
@@ -1,9 +1,9 @@
 -- Use our database
 USE ShopDB; 
-INSERT INTO Orders (CustomerID, DATE) VALUES (1, '2023-01-01');
 
 -- Start the transaction 
 START TRANSACTION; 
+INSERT INTO Orders (CustomerID, DATE) VALUES (1, '2023-01-01');
 INSERT INTO OrderItems (OrderID, ProductID, Count) VALUES (1, 1, 1);
 UPDATE Products SET WarehouseAmount = WarehouseAmount - 1 WHERE ID = 1;
 
